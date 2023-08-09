ebb445fe9ebad64f12c3927f6a0991a51511203d
Jessica Mallian <jlmallian@gmail.com>
2023-08-09 08:41:07 -0500

    Initial Commit

---
+++ SomeApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SomeApp
+//
+//  Created by Jessica Mallian on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SomeApp/SomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeAppApp.swift
+//  SomeApp
+//
+//  Created by Jessica Mallian on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
